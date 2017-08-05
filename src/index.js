2379c3e270b0044048b0e6bbb532dc5669747ba3
agheith <amerghth@gmail.com>
2017-08-05 16:22:18 -0700

    updated video-detail component

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDom from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyAGZcXoTqwxZfE1lA5kIfElcNGA8it6oXQ';
 
 
@@ -10,18 +11,25 @@ class App extends Component{
     constructor(props){
         super(props);
 
-        this.state = { videos: [] };
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
 
         YTSearch ({ key: API_KEY, term: 'Soccer'}, (videos) => {
             console.log(videos);
-            this.setState({ videos: videos })
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            })
         });
     }
     render() {
         return (
             <div>
                 <SearchBar />
-                <VideoList />
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList videos={this.state.videos} />
             </div>
         );
     }
